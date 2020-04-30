645af743b3d7da3223d2fbae68ae6e7da4533ff9
Manuel Gama <mgamamsc@gmail.com>
2020-04-29 21:22:32 -0400

    Setting up barebones components for portfolio sections. Installing react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import WorkExperience from './components/WorkExperience.js';
+import Skills from './components/Skills.js'; 
+import Education from './components/Education.js'; 
+import About from './components/About.js'; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+  
+        // Set up the Router
+        <Route exact path="/" component={About} />
+        <Route path="/work-experience" component={WorkExperience} />
+        <Route path="/education" component={Education} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+                                          
+            // Set up the Links
+            <Link to="/" className="item">About</Link>
+            <Link to="/education" className="item">Education</Link>
+            <Link to="/skills" className="item">Skills</Link>
+            <Link to="/work-experience" className="item">WorkExperience</Link>
+            <Link to="/about" className="item">About</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Education.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function Education(props) {
+  return (
+    <div>
+      <h1>Education</h1>
+    </div>
+  )
+}
+ 
+export default Education;
