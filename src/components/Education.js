51f650886173c31de190ad3ce4db7532516b43a3
Manuel Gama <mgamamsc@gmail.com>
2020-05-09 16:18:36 -0400

    Adding more links and images to Education and Skills components

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,8 +3,8 @@ import React from "react"
 function Education(props) {
   return (
     <div>
+    	<h1>Education</h1>
     	<div>
-	      <h1>Education</h1>
 	      <h2>Strayer University</h2>
 	      <h3>Master of Science in Information Systems with concentration in Software Engineering Management</h3>
 	      <h4>2011 to 2014</h4>
@@ -27,6 +27,30 @@ function Education(props) {
 	      <a href="https://www.ibo.org/about-the-ib/">
 	      	<img src="https://www.ibo.org/Assets/Images/logo-163.svg"/>
 	      </a>
+	    </div>
+	    <h1>Professional Certifications</h1>
+	    <div>
+	      <h2>ISTQB Advanced Level Test Automation Engineer</h2>
+	      <h3>Credential ID: 18-CTAL-TAE-0054-USA</h3>
+	      <a href="https://www.istqb.org/certification-path-root/test-automation-engineer.html">
+	      	<img src="https://aiit.ac.nz/wp-content/uploads/2019/05/300x300-CTAL-TAE.png"/>
+	      </a>
+	    </div>
+	    <div>
+	      <h2>ISTQB Certified Tester Foundation Level</h2>
+	      <h3>Credential ID: 12-CTFL-00852-USA</h3>
+	      <a href="https://www.istqb.org/certification-path-root/foundation-level-2018.html">
+	      	<img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F83484851%2F216232088902%2F1%2Foriginal.20191204-151921?w=1000&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C18%2C478%2C239&s=fa2a8a08924a8cf7767cc45a19f09d5f"/>
+	      </a>
+	    </div>
+	    <h1>Online Courses</h1>
+	    <div>
+	      <h2>Coursera</h2>
+	      <img src="https://149362230.v2.pressablecdn.com/wp-content/uploads/2018/08/coursera-social-logo.png"/>
+	      <a href="https://www.coursera.org/account/accomplishments/verify/AS4PL6MUT7XP">Programming Foundations with JavaScript, HTML and CSS</a>
+	      <a href="https://www.coursera.org/account/accomplishments/verify/Q7A6SN8JHXKP">Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization</a>
+	      <a href="https://www.coursera.org/account/accomplishments/verify/JP5DBJHQAY9C">Structuring Machine Learning Projects</a>
+	      <a href="https://www.coursera.org/account/accomplishments/verify/2TS5M96MKMXP">Neural Networks and Deep Learning</a>
 	    </div>
     </div>
   )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,18 @@ function Skills(props) {
 	<div>
 		<h1>Skills</h1>
 		<div>
+			<h2>Programming Languages</h2>
+			<a href="https://docs.oracle.com/javase/8/docs/technotes/guides/language/index.html">
+				<img src="https://www.oracle.com/a/ocom/img/cb71-java-logo.png"/>
+			</a>
+			<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+				<img src="https://pluralsight2.imgix.net/paths/images/javascript-542e10ea6e.png"/>
+			</a>
+			<a href="https://www.python.org/">
+				<img src="https://python.rs/pylogo.png"/>
+			</a>
+		</div>
+		<div>
 			<h2>Continuous Integration</h2>
 			<a href="https://www.jenkins.io/">
 				<img src="https://miro.medium.com/max/1600/1*LOFbTP2SxXcFpM_qTsUSuw.png"/>
@@ -18,6 +30,9 @@ function Skills(props) {
 			<a href="https://www.heroku.com/">
 				<img src="https://res.cloudinary.com/practicaldev/image/fetch/s--7f5GjxUW--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://thepracticaldev.s3.amazonaws.com/i/c29t9uc8roz8g9rddbqs.png"/>
 			</a>
+			<a href="https://aws.amazon.com/">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1200px-Amazon_Web_Services_Logo.svg.png"/>
+			</a>
 		</div>
 		<div>
 			<h2>Cloud Test Execution</h2>
@@ -27,21 +42,51 @@ function Skills(props) {
 		</div>
 		<div>
 			<h2>Web UI Test Automation</h2>
+			<a href="https://www.selenium.dev/">
+				<img src="https://www.selenium.dev/images/selenium_logo_large.png"/>
+			</a>
 			<a href="https://nightwatchjs.org/">
 				<img src="https://nightwatchjs.org/img/banner.png"/>
 			</a>
 			<a href="https://webdriver.io/">
 				<img src="https://i2.wp.com/grantnorwood.com/app/uploads/2017/07/webdriver-io-logo.png"/>
 			</a>
+			<a href="http://appium.io/">
+				<img src="https://xebialabs.com/wp-content/uploads/files/tool-chest/appium.jpg"/>
+			</a>
+		</div>
+		<div>
+			<h2>Desktop Test Automation</h2>
+			<a href="https://www.froglogic.com/squish/">
+				<img src="https://pythonicways.files.wordpress.com/2016/09/b140e522-60b2-46da-8dd6-b73b41fe4481.png"/>
+			</a>
 		</div>
 		<div>
 			<h2>Android App UI Test Automation</h2>
+			<a href="https://developer.android.com/training/testing/espresso">
+				<img src="https://sigma.software/sites/default/files/blog-espresso-for-android-logo.png"/>
+			</a>
 		</div>
 		<div>
 			<h2>API Test Automation</h2>
+			<a href="https://github.com/intuit/karate">
+				<img src="https://xebialabs.com/wp-content/uploads/2018/10/karate-logo-1.jpg"/>
+			</a>
 		</div>
 		<div>
 			<h2>Unit Test Frameworks</h2>
+			<a href="https://mochajs.org/">
+				<img src="https://danielkorn.io/images/mocha-banner.png"/>
+			</a>
+			<a href="https://testng.org/doc/">
+				<img src="https://i1.wp.com/makeseleniumeasy.com/wp-content/uploads/2018/07/TestNG.png"/>
+			</a>
+			<a href="https://junit.org/junit5/">
+				<img src="https://xebialabs.com/wp-content/uploads/files/tool-chest/junit.jpg"/>
+			</a>
+			<a href="https://docs.pytest.org/en/latest/">
+				<img src="https://miro.medium.com/max/500/1*qmz2bNVJ64273TA4TbFxZw.png"/>
+			</a>
 		</div>
 	</div>
   )
